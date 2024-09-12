4917f3aa6f3e58c30e24e51ebfac82a0a25509de
skyDe <sky_de1991@outlook.com>
2024-09-12 06:57:54 -0800

    LoginForm functionality implemented / not done

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { login } from "./utils";
 import "./styles.css";
 
 // ================ LOGIN FORM ====================
-// 
+//
 // Guidelines:
 // * You have an incomplete login form.
 // * You are not allowed to add any additional HTML elements.
@@ -17,23 +17,70 @@ import "./styles.css";
 //  * Show an alert box (native Javascript alert) if login succeeds. Investigate the login function to find out how to log in successfully.
 
 export default function LoginForm() {
- 
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [fetchStatus, setFechStatus] = useState({
+    isLoading: false,
+    isError: false,
+    errMessage: "",
+  });
+
+  // const handleSubmit = () => login({ email, password });
+  const handleSubmit = () => {
+    // console.log(formState);
+    setFechStatus({ ...fetchStatus, isLoading: true });
+    login({ email: formState.email, password: formState.password })
+      .then((res) => {})
+      .catch((err) => {
+        setFechStatus({
+          ...fetchStatus,
+          isLoading: false,
+          isError: true,
+          errMessage: err.message,
+        });
+      });
+  };
+  const handleChange = (e) => {
+    console.log(e.target.name);
+
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="wrapper">
       <div className="row">
         <label htmlFor={"email"}>Email</label>
-        <input id={"email"} type={"email"}  />
+        <input
+          id={"email"}
+          type={"email"}
+          name="email"
+          value={formState.email}
+          onChange={handleChange}
+        />
       </div>
       <div className="row">
         <label htmlFor={"password"}>Password</label>
-        <input id={"password"} type={"password"}/>
+        <input
+          id={"password"}
+          type={"password"}
+          value={formState.password}
+          name="password"
+          onChange={handleChange}
+        />
       </div>
 
       {/* Place login error inside this div. Show the div ONLY if there are login errors. */}
-      <div className="errorMessage"></div>
+      {fetchStatus.isError && (
+        <div className="errorMessage">{fetchStatus.errMessage}</div>
+      )}
 
       <div className="button">
-        <button>Login</button>
+        <button disabled={fetchStatus.isLoading} onClick={handleSubmit}>
+          Login
+        </button>
       </div>
     </div>
   );
